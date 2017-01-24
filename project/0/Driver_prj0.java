4dbf0f8025843ce2d960e4e735335657a1514aac
calydiscope <calydiscope@gmail.com>
2017-01-23 20:51:17 -0500

    Project 0 Assignment.

---
+++ project/0/Driver_prj0.java
@@ -0,0 +1,21 @@
+/**
+* file: Driver_prj0.java
+* author: Lauren Urena-Clark
+* course: CMPT 220
+* assignment: project 0
+* due date: January 24, 2017
+
+*This file contains the declaration of the 
+*Driver_prj0 abstract data type.
+*/
+/**
+
+* Driver_prj0
+*
+* This class says "Hello World!"
+*/
+public class Driver_prj0 {
+public static void main(String[] args) {
+System.out.println("Hello World!");
+  }
+}
